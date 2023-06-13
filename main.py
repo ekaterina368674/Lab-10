fc4a61cb624c098acd87d44e809ff5abcc662818
ekaterina368674 <114882735+ekaterina368674@users.noreply.github.com>
2023-06-13 16:10:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
-import json, os
+import os
+import pyaudio
+import pymorphy2
+import pyttsx3
+import requests
+import translators as ts
+import vosk
+from random import choice
 
-import pyttsx3, pyaudio, vosk
 
-tts = pyttsx3.init('sapi5')
-
-voices = tts.getProperty('voices')
-tts.setProperty('voices', 'en')
-
-for voice in voices:
-    print(voice.name)
-    if voice.name == 'Microsoft Zira Desktop - English (United States)':
-        tts.setProperty('voice', voice.id)
-
-model = vosk.Model('model_small')
-record = vosk.KaldiRecognizer(model, 16000)
-pa = pyaudio.PyAudio()
-stream = pa.open(format=pyaudio.paInt16,
-                 channels=1,
-                 rate=16000,
-                 input=True,
-                 frames_per_buffer=8000)
-stream.start_stream()
+def get_currency_dict() -> dict:
+    return requests.get(
+        'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/rub.json',
+        timeout=10
+    ).json()
 
 
 def listen():
@@ -37,12 +29,42 @@ def speak(say):
     tts.runAndWait()
 
 
-speak('starting')
-print('start...')
-for text in listen():
-    if text == 'закрыть':
-        quit()
-    elif text == 'блокнот':
-        os.system('notepad.exe')
-    else:
+if __name__ == '__main__':
+    morph = pymorphy2.MorphAnalyzer(lang='ru')
+    tts = pyttsx3.init('sapi5')
+    voices = tts.getProperty('voices')
+    tts.setProperty('voices', 'ru')
+    tts.setProperty('voice', next(filter(lambda voice: 'ru' in voice.name.lower(), voices)).id)
+    model = vosk.Model(next(filter(lambda file_folder: 'ru' in file_folder, os.listdir())))
+    record = vosk.KaldiRecognizer(model, 16000)
+    pa = pyaudio.PyAudio()
+    stream = pa.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+    stream.start_stream()
+
+    speak('День добрый')
+    for text in listen():
         print(text)
+        text = morph.parse(text)[0].normal_form
+        if text == 'доллар':
+            speak(f'Доллар стоит {1 / get_currency_dict()["rub"]["usd"]:.2f} рублей')
+        elif text == 'евро':
+            speak(f'Евро стоит {1 / get_currency_dict()["rub"]["usd"]:.2f} рублей')
+        elif text == 'сохранить':
+            data = get_currency_dict()
+            with open(f'currency_{data["date"]}.csv', 'w', encoding='utf-8') as f:
+                f.write('name,price\n')
+                for key in data["rub"]:
+                    f.write(f'{key},{data["rub"][key]}\n')
+            speak('Файл сохранен')
+        elif text == 'случайный':
+            data = get_currency_dict()["rub"]
+            key = choice(data.keys())
+            if data[key] < 1.000001:
+                speak(f'{key} стоит {1 / data[key]:.2f} рублей')
+            else:
+                speak(f'Рубль стоит {data[key]:2.f} {key}')
+        elif text == 'стоп':
+            speak('Всего доброго')
+            break
+        else:
+            speak('Жду команды')
